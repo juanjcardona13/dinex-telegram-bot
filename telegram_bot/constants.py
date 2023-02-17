06c32b5e4b938afcf835d86b290fa4e05ee3b7bc
juanjcardona13 <45944359+juanjcardona13@users.noreply.github.com>
2023-02-16 22:47:12 -0500

    Update constants.py

--- telegram_bot/constants.py
+++ telegram_bot/constants.py
@@ -1,10 +1,6 @@
 import os
 from telegram.ext import (ConversationHandler)
 
-<<<<<<< HEAD
-=======
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "YOUR_TOKEN")
->>>>>>> 1e11dbe54641d5a296873714a70e4ad3750716c1
 API_BASE = os.getenv("API_BASE", "https://framex-dev.wadrid.net/api/")
 VIDEO_NAME = os.getenv(
     "VIDEO_NAME", "Falcon Heavy Test Flight (Hosted Webcast)-wbSwFU6tY1c"
